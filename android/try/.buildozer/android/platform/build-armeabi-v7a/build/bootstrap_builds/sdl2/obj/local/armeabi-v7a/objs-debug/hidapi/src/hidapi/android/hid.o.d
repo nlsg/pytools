154d10af41e9307b7eb7faf0e2358141521a54ce
nls <kappeler.nils@gmail.com>
2021-11-13 15:50:40 +0100

    testing toolchain

--- android/try/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/hidapi/src/hidapi/android/hid.o.d
+++
@@ -1,5 +0,0 @@
-/home/nls/py/android/try/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/hidapi/src/hidapi/android/hid.o: \
-  /home/nls/py/android/try/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/hid.cpp \
-  /home/nls/py/android/try/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h
-
-/home/nls/py/android/try/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h:
