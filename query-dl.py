0208dd38dd9b3898ef42ff88f994cb4c44bdab7e
nls <kappeler.nils@gmail.com>
2022-12-08 18:45:01 +0100

    minor updates

--- query-dl.py
+++ query-dl.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 HEADER = """
-                                       _ _ 
-  __ _ _   _  ___ _ __ _   _        __| | |
- / _` | | | |/ _ \ '__| | | |_____ / _` | |
-| (_| | |_| |  __/ |  | |_| |_____| (_| | |
- \__, |\__,_|\___|_|   \__, |      \__,_|_|
-    |_|                |___/               
+                                       _ _               
+  __ _ _   _  ___ _ __ _   _        __| | |  _ __  _   _ 
+ / _` | | | |/ _ \ '__| | | |_____ / _` | | | '_ \| | | |
+| (_| | |_| |  __/ |  | |_| |_____| (_| | |_| |_) | |_| |
+ \__, |\__,_|\___|_|   \__, |      \__,_|_(_) .__/ \__, |
+    |_|                |___/                |_|    |___/ 
 
+accepts a file containing youtube search queries as argument 
+and downloads them ( parallely ) with the youtube-dl API 
+
+standalone usage:
 $ query-dl.py file | -h
+simple API:
+>>>from query-dl import download
+>>>download("search-query") # will download to current working directory
 file is a file containing youtube search queries separated by newlines
 """
+
 from sys import argv
 from os import popen
 from multiprocessing import Pool
@@ -31,7 +39,7 @@ def downloadIO(query):
   return 
 
 if __name__ == "__main__":
-  if argv[1] == "-h":
+  if len(argv) == 1 or argv[1] == "-h":
     print(HEADER)
     exit()
   queries = map(lambda x:x[:-1],open(argv[1],"r").readlines())
